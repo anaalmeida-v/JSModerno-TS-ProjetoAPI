c9d62b66adad53fdacad2d754a76609d1927feeb
brasiliaamarela <aninhaalmeidav@gmail.com>
2023-07-19 11:49:57 -0300

    Destructuring

--- scripts.js
+++ scripts.js
@@ -136,3 +136,21 @@ const userName = 'Matheus'
 const age = 30
 console.log(`O nome do usuário é: ${userName} e ele tem ${age} anos.`)
 console.log("O nome do usuário é: " + userName + "e ele tem " + age + " anos.")
+
+//aula 6 - destructuring
+const fruits = ["Maçã", "Laranja", "Mamão"]
+
+const [f1, f2, f3] = fruits
+console.log(f1, f2)
+console.log(f3)
+
+const productDetails = {
+    name: "Mouse",
+    price: 39.99,
+    category: "Periféricos",
+    color: "Cinza"
+}
+
+const { name: productName, price, category: productCategory, color } = productDetails
+
+console.log(`O nome do produto é ${productName}, custa R$${price}, pertence a categoria ${productCategory} e é da cor ${color}`)
