69dc04414879efccefcf31da41ae5ced724fb65a
brasiliaamarela <aninhaalmeidav@gmail.com>
2023-07-19 12:18:03 -0300

    Spread operator

--- scripts.js
+++ scripts.js
@@ -154,3 +154,20 @@ const productDetails = {
 const { name: productName, price, category: productCategory, color } = productDetails
 
 console.log(`O nome do produto é ${productName}, custa R$${price}, pertence a categoria ${productCategory} e é da cor ${color}`)
+
+//aula 7 - spread operator
+const a1 = [1, 2, 3]
+const a2 = [4, 5, 6]
+
+const a3 = [...a1, ...a2]
+console.log(a3)
+
+const a4 = [0, ...a1, 4]
+console.log(a4)
+
+const carName = { name: "Gol" }
+const carBrand = { brand: "VW" }
+const otherInfos = { km: 100000, price: 49000 }
+
+const car = { ...carName, ...carBrand, ...otherInfos, wheels: 4 }
+console.log(car)
