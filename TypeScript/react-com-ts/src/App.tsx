90a723254fff9804cc0ae832204329674957b1a0
brasiliaamarela <aninhaalmeidav@gmail.com>
2023-07-19 16:22:53 -0300

    variáveis no react

--- TypeScript/react-com-ts/src/App.tsx
+++ TypeScript/react-com-ts/src/App.tsx
@@ -1,8 +1,15 @@
-
 function App() {
+  //1 - variáveis
+  const name: string = "Matheus";
+  const age: number = 30;
+  const isWorking: boolean = true;
+
   return (
     <div className="App">
-      |<h1>TypeScript com React</h1>
+      <h1>TypeScript com React</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && <p>Está trabalhando!</p>}
     </div>
   );
 }
