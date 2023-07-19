636b2a9d5439b86fe846344f22ca41963d6f9fd5
brasiliaamarela <aninhaalmeidav@gmail.com>
2023-07-19 18:17:04 -0300

    functions

--- TypeScript/react-com-ts/src/App.tsx
+++ TypeScript/react-com-ts/src/App.tsx
@@ -4,12 +4,19 @@ function App() {
   const age: number = 30;
   const isWorking: boolean = true;
 
+  //2 - funções
+  const userGreeting = (name: string): string => {
+    return `Olá, ${name}`;
+  };
+
   return (
     <div className="App">
       <h1>TypeScript com React</h1>
       <h2>Nome: {name}</h2>
       <p>Idade: {age}</p>
       {isWorking && <p>Está trabalhando!</p>}
+      <h3>{userGreeting(name)}</h3>
+      {/* se eu tentar colocar o age por exemplo, a variável será recusada, por ser um number e não uma string */}
     </div>
   );
 }
