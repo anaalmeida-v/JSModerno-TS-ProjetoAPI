d8108157d7554aa724086a29e3db2f6d8a08cd14
aninha <aninha@teste.com.br>
2023-07-20 13:21:56 -0300

    Context API

--- TypeScript/react-com-ts/src/App.tsx
+++ TypeScript/react-com-ts/src/App.tsx
@@ -1,3 +1,5 @@
+import React, { createContext } from "react";
+
 //4 - importação de components
 import FirstComponent from "./components/FirstComponent";
 
@@ -11,7 +13,18 @@ import State from "./components/State";
 //8 - type
 type textOrNull = string | null;
 
-type fixed = "Isso" | "Ou" | "Aquilo"
+type fixed = "Isso" | "Ou" | "Aquilo";
+
+//9 - context
+interface IAppContext {//interface - declarando tipos para variáveis
+  language: string;
+  framework: string;
+  projects: number;
+}
+
+//function context
+export const AppContext = createContext<IAppContext | null>(null); //precisa ser exportado para poder ser acessado
+//criando context que recebe valor de dado suportado
 
 function App() {
   //1 - variáveis
@@ -26,31 +39,41 @@ function App() {
 
   //8 - type
   const myText: textOrNull = "Tem algum texto aqui";
-  let mySecondText: textOrNull = null
+  let mySecondText: textOrNull = null;
 
-  mySecondText = "opa"
-  const testandoFixed: fixed = "Isso"
+  mySecondText = "opa";
+  const testandoFixed: fixed = "Isso";
+
+  //9 - context
+  const contextValue: IAppContext = {//obj
+    language: "JavaScript",
+    framework: "Express",
+    projects: 5,
+  };
+
   return (
-    <div className="App">
-      <h1>TypeScript com React</h1>
-      <h2>Nome: {name}</h2>
-      <p>Idade: {age}</p>
-      {isWorking && <p>Está trabalhando!</p>}
-      <h3>{userGreeting(name)}</h3>
-      {/* se eu tentar colocar o age por exemplo, a variável será recusada, por ser um number e não uma string */}
-      <FirstComponent />
-      <SecondComponent name="Segundo" />
-      <Destructuring
-        title="Primeiro post"
-        content="Algum conteúdo"
-        commentsQty={10}
-        tags={["ts", "js"]}
-        category={Category.TS}
-      />
-      <State />
-      { myText && <p>Tem texto na variável</p>}
-      { mySecondText && <p>Tem texto na variável</p>}
-    </div>
+    <AppContext.Provider value={contextValue}>
+      <div className="App">
+        <h1>TypeScript com React</h1>
+        <h2>Nome: {name}</h2>
+        <p>Idade: {age}</p>
+        {isWorking && <p>Está trabalhando!</p>}
+        <h3>{userGreeting(name)}</h3>
+        {/* se eu tentar colocar o age por exemplo, a variável será recusada, por ser um number e não uma string */}
+        <FirstComponent />
+        <SecondComponent name="Segundo" />
+        <Destructuring
+          title="Primeiro post"
+          content="Algum conteúdo"
+          commentsQty={10}
+          tags={["ts", "js"]}
+          category={Category.TS}
+        />
+        <State />
+        {myText && <p>Tem texto na variável</p>}
+        {mySecondText && <p>Tem texto na variável</p>}
+      </div>
+    </AppContext.Provider>
   );
 }
 
