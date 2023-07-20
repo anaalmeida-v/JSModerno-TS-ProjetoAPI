a264eb07dd273eaa18ecfc3371fe7c60ab9a0d18
aninha <aninha@teste.com.br>
2023-07-20 14:24:36 -0300

    Criando header e footer

--- todo_react/src/App.tsx
+++ todo_react/src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
+
+//components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Todo React</h1>
+    <div>
+      <Header />
+      <h1>Conteúd...</h1>
+      <Footer />
     </div>
   );
 }
---
+++ todo_react/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        <span>React + TS Todo</span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
