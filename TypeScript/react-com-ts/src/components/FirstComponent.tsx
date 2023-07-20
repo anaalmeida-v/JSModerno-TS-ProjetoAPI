d27d339a0df3e31d51e3cea5114dd463911de101
Ana <aninha@teste.com.br>
2023-07-20 10:22:02 -0300

    Criando componentes - extensão

---
+++ TypeScript/react-com-ts/src/components/FirstComponent.tsx
@@ -0,0 +1,14 @@
+// 3 - component
+import React from 'react'
+
+type Props = {}
+
+const FirstComponent = () => {
+  return (
+    <div>
+        <h1>Meu Primeiro Componente</h1>
+    </div>
+  )
+}
+
+export default FirstComponent
