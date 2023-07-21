6d770c0153f8d738263b528556e99720c6617c2e
aninhaalmeidav <aninha@teste.com.br>
2023-07-21 18:12:31 -0300

    home

--- movies_lib/src/pages/Home.jsx
+++ movies_lib/src/pages/Home.jsx
@@ -1,5 +1,25 @@
+import { useState, useEffect } from "react";
+
+//chave e url da api
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    console.log(data);  
+  };
+
+  useEffect(() => {
+    const TopRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+
+    getTopRatedMovies(TopRatedUrl);
+  }, []);
+
   return <div>Home</div>;
 };
 
