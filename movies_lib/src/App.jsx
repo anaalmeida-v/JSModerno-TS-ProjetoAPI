8086443282e2096183a64daa64cc2064e711d090
aninhaalmeidav <aninha@teste.com.br>
2023-07-21 17:47:27 -0300

    body navbar

--- movies_lib/src/App.jsx
+++ movies_lib/src/App.jsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import { Outlet } from "react-router-dom";
+
+//components
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App">
-    <h2>Movies Lib</h2>
-  </div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
--- movies_lib/src/pages/Movie.jsx
+++ movies_lib/src/pages/Movie.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 
 const Movie = () => {
   return <div>Movie</div>;
